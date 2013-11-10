7f4f185b5975fc6d45b05eabfc38a8a6058ff5fc
Olekhandro <olekhandro@rambler.ru>
2013-11-10 15:30:01 +0400

    Added new form

--- EteraShopDownloadingApplication/EteraShopCartApplication/GoodAtCart.cs
+++ EteraShopDownloadingApplication/EteraShopCartApplication/GoodAtCart.cs
@@ -8,8 +8,8 @@ namespace EteraShopCartApplication
     public class GoodAtCart
     {
         public string Title { get; set; }
-        public string Price { get; set; }
-        public string Description { get; set; }
         public string Option { get; set; }
+        public int Number { get; set; }
+        public string Status { get; set; }
     }
 }
